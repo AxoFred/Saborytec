e2225ea99ed43c4121d954b219a1b1075a6c7050
Fredy Cano Lopez <l22240046@smartin.tecnm.mx>
2025-05-12 16:15:10 -0600

    logeo/bienvenida

---
+++ Bienvenida.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>SaboryTec</title>
+    <link rel="stylesheet" href="css/stylePrincipal.css">
+</head>
+<body class="elbody">
+    <header>
+        <div class="tit">
+            <h1>SaboryTec<img src="img/logo-llama.png" width="40" height="40" alt="logo"><br></h1>
+        </div>
+        
+        <nav class="menu">
+            <ul class="orientacionUL">
+                <li><a href="#">COMIDA</a></li>
+                <li><a href="#">PRODUCTOS</a></li>
+                <li><a href="#">BEBIDAS</a></li>
+                <li><a href="#">OTROS</a></li>
+            </ul>
+            <div class="menu2">
+                <ul class="orientacionUL2">
+                    <li><a href="Logeo.html">INICIAR SESIÓN</a></li>
+                    <li><a href="#">FAVORITOS</a></li>
+                    <li><a href="#">CARRITO(0)</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <div>
+        <h2 class="texto1">Bienvenido a SaboryTec</h2>
+    </div>
+
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css\style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>SaboryTec</title>
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <div class="fondo">
+            <div class="titulo">
+                <h1>
+                    SaboryTec
+                </h1>
+            </div>
+            <div class="inicio">
+                <h2>
+                Inicia sesión
+                </h2>
+            </div>
+            <div class=formulario>
+            <h6>Correo institucional</h6>
+                <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="" placeholder="correo">
+                        <label for="floatingInput">Ingresa su correo</label>
+                </div>
+                <h6>Contraseña</h6>
+                <div class="form-floating">
+                        <input type="password" class="form-control" id="" placeholder="Pcontraseña">
+                        <label for="floatingPassword">Ingresa su contraseña</label>
+                </div>
+                <a href="#">¿Olvidaste tu contraseña?</a>
+                <div class="botton">
+                    <button type="submit">Iniciar Sesión</button>
+                </div>
+
+                <div class="crear">
+                    <h6>Registrarse? <a href="#">Crear Cuenta</a></h6>
+                </div>
+            </div>
+      </div>
+        <!--segunda parte-->
+      <div class="fondo1">
+        
+      </div>
+</body>
+</html>
